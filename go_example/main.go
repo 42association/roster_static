cb34249ee6fbfa4d4661a0ecec5dd2869326701e
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-06 17:37:23 +0900

    :sparkles:[add]routing (list_have_to, list_did)

--- go_example/main.go
+++ go_example/main.go
@@ -23,15 +23,15 @@ func main() {
             "day":   time.Now().Format(stdZeroDay),
         })
     })
-//     router.GET("/list_did.html", func(c *gin.Context) {
-//         c.HTML(200, "list_did.html", gin.H{
-//             "haveto": []string{"Michael", "Dwight", "Jim", "Pam", "Angela", "Oscar", "Kevin"},
-//             "did":    []string{"Stanley", "Phyllis", "Andy", "Robert", "Ryan", "Kelly", "Toby"},
-//             "date":   time.Now().Format("2006-01-02"),
-//             "month":   time.Now().Format(stdZeroMonth),
-//             "day":   time.Now().Format(stdZeroDay),
-//         })
-//     })
-
+    router.GET("/list_did.html", func(c *gin.Context) {
+        c.HTML(200, "list_did.html", gin.H{
+            "did":    []string{"Stanley", "Phyllis", "Andy", "Robert", "Ryan", "Kelly", "Toby"},
+        })
+    })
+    router.GET("/list_have_to.html", func(c *gin.Context) {
+        c.HTML(200, "list_did.html", gin.H{
+            "haveto":    []string{"Stanley", "Phyllis", "Andy", "Robert", "Ryan", "Kelly", "Toby"},
+        })
+    })
     router.Run(":8040")
 }
